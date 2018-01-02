410deeb0437469f9280b36c5c1d29bb775bba49b
Marcel Dancak <dancakm@gmail.com>
2018-01-02 13:10:08 +0100

    fixed function for formatting values (MS Edge)

--- src/components/identification/format.js
+++ src/components/identification/format.js
@@ -3,8 +3,10 @@ export default function formatValue(value, params = {}) {
   if (Array.isArray(value)) {
     value = value.join(', ');
   }
-  if (params.type === 'number') {
-    value = Intl.NumberFormat.apply(null, params.format || []).format(value)
+  if (params.type === 'map') {
+    value = params.map[value] || '-';
+  } else if (params.type === 'number') {
+    value = Intl.NumberFormat.apply(Intl, params.format || []).format(value);
   }
   if (params.template) {
     value = params.template.replace(/\$\{value\}/g, value.toString());
